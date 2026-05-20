7469bc12cde3b2e77bc2922155ac8704948f47f5
Gabriel Fontes <hi@m7.rs>
2026-05-20 18:27:46 -0300

    add support for mfc, fix parsing issue with CN, refactor

--- get_cards.py
+++ get_cards.py
@@ -2,7 +2,8 @@
 #! nix-shell -i python
 #! nix-shell -p upscayl-ncnn "python3.withPackages(p: [p.requests])"
 
-import io
+from urllib3.util import Url
+from pathlib import Path
 import subprocess
 from tempfile import TemporaryDirectory
 from dataclasses import dataclass
@@ -12,6 +13,12 @@ import re
 import requests
 
 
+def download_file(url: Url, target: Path):
+    r = requests.get(url)
+    r.raise_for_status()
+    open(target, "wb").write(r.content)
+
+
 @dataclass
 class CardEntry:
     qty: Optional[int]
@@ -19,7 +26,7 @@ class CardEntry:
     setcode: Optional[str]
     collector_number: Optional[str]
     PATTERN = re.compile(
-        r'^"?(?:(\d+)(?:x)? )?([^()]+?)(?: \((\w+)\))?(?: (?:#)?(\d+))?"?$'
+        r'^"?(?:(\d+)(?:x)? )?([^()]+?)(?: \((\w+)\)(?: ([^" ]+))?)?"?$'
     )
 
     @classmethod
@@ -32,61 +39,103 @@ class CardEntry:
             return None
 
     def to_card(self) -> Optional["Card"]:
+        # Try first with exact CN and set
         if self.collector_number and self.setcode:
-            return Card.new(self.setcode, self.collector_number)
+            r = requests.get(
+                f"https://api.scryfall.com/cards/{self.setcode}/{self.collector_number}"
+            )
+            if not r:
+                return None
+            data = r.json()
+        # If not, search by name and optionally set
         elif self.name:
-            return Card.new_named(self.name, self.setcode)
+            r = requests.get(
+                "https://api.scryfall.com/cards/named",
+                params={"exact": self.name, "set": self.setcode},
+            )
+            data = r.json()
+            if not r:
+                return None
+        # Else, we couldn't find it
         else:
             return None
+        # Double sided
+        if "card_faces" in data:
+            front = data["card_faces"][0]
+            name_front = front["name"]
+            image_uri_front = front["image_uris"]["png"]
+            back = data["card_faces"][1]
+            name_back = back["name"]
+            image_uri_back = back["image_uris"]["png"]
+        # Single sided
+        else:
+            name_front = data["name"]
+            image_uri_front = data["image_uris"]["png"]
+            name_back = None
+            image_uri_back = None
+        return Card(
+            name_front=name_front,
+            image_uri_front=image_uri_front,
+            name_back=name_back,
+            image_uri_back=image_uri_back,
+            setcode=data["set"],
+            collector_number=data["collector_number"],
+        )
 
 
 @dataclass
 class Card:
-    name: str
+    name_front: str
+    image_uri_front: str
+    name_back: Optional[str]
+    image_uri_back: Optional[str]
     setcode: str
     collector_number: str
-    image_uri: str
 
-    @classmethod
-    def new(cls, setcode: str, collector_number: str) -> Optional["Card"]:
-        r = requests.get(f"https://api.scryfall.com/cards/{setcode}/{collector_number}")
-        r.raise_for_status()
-        data = r.json()
-        return Card(
-            data["name"],
-            data["set"],
-            data["collector_number"],
-            data["image_uris"]["png"],
-        )
-
-    @classmethod
-    def new_named(cls, name: str, setcode: Optional[str]) -> Optional["Card"]:
-        r = requests.get(
-            "https://api.scryfall.com/cards/named",
-            params={
-                "exact": name,
-                "set": setcode,
-            },
-        )
-        if r:
-            data = r.json()
-            return Card(
-                data["name"],
-                data["set"],
-                data["collector_number"],
-                data["image_uris"]["png"],
+    def download_faces(self, output_dir: str):
+        directory = Path(output_dir)
+        download_file(Url(self.image_uri_front), directory / (self.name_front + ".png"))
+        if self.image_uri_back and self.name_back:
+            download_file(
+                Url(self.image_uri_back), directory / (self.name_back + ".png")
             )
+
+
+def parse_input() -> list[CardEntry]:
+    entries: list[CardEntry] = []
+    print("Parsing input", end="", flush=True)
+    for line in fileinput.input():
+        line = line.strip()
+        if entry := CardEntry.parse(line):
+            print(entry)
+            print(".", end="", flush=True)
+            entries.append(entry)
+        elif line == "":
+            # Skip empty line
+            continue
         else:
-            return None
+            raise Exception(f"Invalid line '{line}'")
+    print("done")
+    return entries
+
+
+def fetch_cards(entries: list[CardEntry]) -> list[Card]:
+    cards: list[Card] = []
+    print("Fetching card data", end="", flush=True)
+    for entry in entries:
+        if card := entry.to_card():
+            print(".", end="", flush=True)
+            cards.append(card)
+        else:
+            raise Exception(f"Failed to find card {entry.name}")
+    print("done")
+    return cards
 
 
 def download_cards(cards: list[Card], directory: str):
     print("Downloading cards", end="", flush=True)
     for card in cards:
-        filename = directory + "/" + card.name + ".png"
-        r = requests.get(card.image_uri)
-        r.raise_for_status()
-        open(filename, "wb").write(r.content)
+        card.download_faces(directory)
         print(".", end="", flush=True)
     print("done")
 
@@ -100,21 +149,18 @@ def upscayl(input_path: str, output_path: str, model: str):
             if "Upscayled Successfully!" in line:
                 print(".", end="", flush=True)
     print("done")
+    print("Cards saved to", output_path)
 
 
 def main():
-    cards: list[Card] = []
-    print("Parsing input...")
-    for line in fileinput.input():
-        if entry := CardEntry.parse(line):
-            if card := entry.to_card():
-                cards.append(card)
-    print("Found:", cards)
+    entries = parse_input()
+    cards = fetch_cards(entries)
     with TemporaryDirectory(delete=True, dir="/tmp") as tmp:
         download_cards(cards, tmp)
-        with TemporaryDirectory(delete=False, dir="/tmp") as upscaled_dir:
+        with TemporaryDirectory(
+            prefix="upscaled_cards", delete=False, dir="/tmp"
+        ) as upscaled_dir:
             upscayl(tmp, upscaled_dir, "realesrgan-x4plus-anime")
-            print("Cards saved to", upscaled_dir)
 
 
 if __name__ == "__main__":
